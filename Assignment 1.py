88497220c98aa7736f9bae94e614bc991ab5c124
Mahawa_Kourouma <mawakou@icloud.com>
2025-03-13 20:39:14 -0400

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,41 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+sll = SinglyLinkedList()
+
+sll.append("A")
+sll.append("B")
+sll.append("C")
+
+sll.prepend("D")
+
+sll.print_list()
